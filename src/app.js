34814377febe8c6baef420b2098a73cbb1d75713
StephanieVA <stephanie1104@outlook.com>
2022-11-13 05:03:47 -0500

    instlando el cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import express from 'express'
 import productRoutes from './routes/product.routes.js'
-import indexRoutes from './routes/index.routers.js'
 import categoryRoutes from './routes/category.routes.js'
 
 const app = express()
 
-app.use('/api',indexRoutes)
 app.use('/api',categoryRoutes)
 app.use('/api',productRoutes)
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import app from './app.js'
 import {PORT} from './config.js'
+const cors = require('cors')
+
+app.use(cors())
 
 app.listen(PORT)
 console.log('Server running on port '+ PORT)
